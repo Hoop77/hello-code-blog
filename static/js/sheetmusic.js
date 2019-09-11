0ecd5f746e8e68d948fc0d19811b87c303bb4a2e
Hoop77 <p.badenhoop@gmx.de>
2019-09-11 10:42:54 +0200

    -

--- static/js/sheetmusic.js
+++ static/js/sheetmusic.js
@@ -3,7 +3,7 @@ $(document).ready(function()
     $('.sheetmusic').each(function(idx, el) {
         var file = $(el).data('src')
         var mode = $(el).data('mode')
-        var osmd = new opensheetmusicdisplay.OpenSheetMusicDisplay(el, {autoResize: false});
+        var osmd = new opensheetmusicdisplay.OpenSheetMusicDisplay(el, {autoResize: true});
         if (mode === 'simple')
         {
             osmd.setOptions({
